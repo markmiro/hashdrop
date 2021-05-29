983582eed5b0e6e1e4c330409426d50f663a0db1
Mark Miro <contact@markmiro.com>
2021-05-29 00:28:29 -0700

    Clean up UX of verify page

--- pages/verify.tsx
+++ pages/verify.tsx
@@ -1,16 +1,64 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import styles from "../styles/Verify.module.css";
+import queryString from "query-string";
 const Hash = require("ipfs-only-hash");
+
+function getQueryStringHash() {
+  if (!window.location.search) return "";
+  const { hash } = queryString.parse(window.location.search);
+  if (typeof hash !== "string") return "";
+  return hash;
+}
+
+function hashToUrl(hash: string) {
+  return `https://${hash}.ipfs.dweb.link`;
+}
 
 export default function Verify() {
   const fileRef = useRef<HTMLInputElement>(null);
+  const [hasLocationSearch, setHasLocationSearch] = useState(false);
   const [text, setText] = useState("");
+  const [wantedHash, setWantedHash] = useState("");
   const [hash, setHash] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [isValid, setIsValid] = useState<boolean | null>(null);
+  const [hashFound, setHashFound] = useState(false);
+  const [wantedHashFound, setWantedHashFound] = useState(false);
+
+  const checkWantedHash = useCallback(
+    async (wantedHash: string) => {
+      try {
+        await fetch(hashToUrl(wantedHash));
+        setWantedHashFound(true);
+      } catch (err) {
+        setWantedHashFound(false);
+      }
+    },
+    [wantedHash]
+  );
+
+  useEffect(() => {
+    setWantedHash(getQueryStringHash());
+    if (window.location.search) setHasLocationSearch(true);
+  }, []);
+
+  useEffect(() => {
+    checkWantedHash(wantedHash);
+  }, [wantedHash]);
+
+  const calcHash = useCallback(async (text: string) => {
+    setHashFound(false);
+    setHash("");
+    const ipfsTextHash = await Hash.of(text, { cidVersion: 1 });
+    setHash(text ? ipfsTextHash : "");
+    try {
+      await fetch(hashToUrl(ipfsTextHash));
+      setHashFound(true);
+    } catch (err) {
+      setHashFound(false);
+    }
+  }, []);
 
   function loadFile(e: any) {
-    const files = e.target.files;
+    const files: File[] = e.target.files;
     if (files && files[0]) {
       const reader = new FileReader();
       reader.onload = (e) => {
@@ -18,60 +66,115 @@ export default function Verify() {
         console.log(contents);
         setText(contents as string);
         calcHash(contents as string);
+        // alert(files[0].name);
       };
       reader.readAsText(files[0]);
     }
   }
 
-  const calcHash = useCallback(async (text: string) => {
-    setLoading(true);
-    const ipfsTextHash = await Hash.of(text, { cidVersion: 1 });
-    setHash(text ? ipfsTextHash : "");
-    setLoading(false);
-  }, []);
-
   function reset() {
-    if (!fileRef.current) return;
-    fileRef.current.type = "text";
-    fileRef.current.type = "file";
-    fileRef.current.setAttribute("value", "");
+    if (fileRef.current) {
+      fileRef.current.type = "text";
+      fileRef.current.type = "file";
+      fileRef.current.setAttribute("value", "");
+    }
     setText("");
     setHash("");
+    setHashFound(false);
   }
 
-  useEffect(() => {
-    if (!window.location.search) return;
-    const isValid = window.location.search === "?hash=" + hash;
-    setIsValid(isValid);
-  }, [hash]);
+  const isValid = wantedHash && hash === wantedHash;
 
   return (
     <div>
-      <div className={styles.fileDrop}>
-        <div>
-          Drop a file here
-          <br />
-          <button>Open File</button>
-        </div>
+      <label>
+        Expected hash:
         <input
-          className={styles.fileDropInput}
-          type="file"
-          ref={fileRef}
-          onChange={loadFile}
+          className={styles.textInput}
+          placeholder="Paste in expected hash"
+          value={wantedHash}
+          onChange={(e) => setWantedHash(e.target.value)}
+          autoCorrect="off"
+          autoCapitalize="off"
+          autoFocus
+          disabled={hasLocationSearch}
         />
-      </div>
-      {text && hash && (
+      </label>
+      {wantedHash && (
         <>
-          <button onClick={reset}>Reset</button>
-          {window.location.search && <div>{isValid ? "VALID" : "INVALID"}</div>}
-          <pre className={styles.text}>{text}</pre>
-          {hash}
-          <iframe
-            src={`https://${hash}.ipfs.dweb.link/`}
-            width="100%"
-            height="500px"
-          />
+          {wantedHashFound ? (
+            <>
+              From IPFS:
+              <iframe src={hashToUrl(wantedHash)} width="100%" height="500px" />
+            </>
+          ) : (
+            <div className={styles.warningBox}>
+              File for hash not published, or file published but cannot be
+              found.
+              <p>
+                If you're the one who created the hash above, publish the file
+                using something like <a href="https://pinata.cloud/">Pinata</a>{" "}
+                once you're ready to share your secret message with the world.
+              </p>
+            </div>
+          )}
         </>
+      )}
+      <br />
+      <br />
+      {text && hash ? (
+        <>
+          {isValid ? (
+            <div className={styles.successBox}>
+              <button onClick={reset}>← Choose another file</button>
+              <h1>✅ Perfect Match</h1>
+              Dropped file hash: {hash}
+            </div>
+          ) : (
+            <div className={styles.actualFileArea}>
+              <div className={styles.errorBox}>
+                <button onClick={reset}>← Back</button>
+                <h1>❌ Hash Mismatch</h1>
+                <pre style={{ textAlign: "left" }}>
+                  Wanted: {wantedHash}
+                  <br />
+                  Actual: {hash}
+                </pre>
+              </div>
+              <br />
+              Contents:
+              <pre className={styles.text}>{text}</pre>
+              <br />
+              {hashFound ? (
+                <>
+                  From IPFS:
+                  <iframe src={hashToUrl(hash)} width="100%" height="500px" />
+                </>
+              ) : (
+                <div className={styles.errorBox}>
+                  File for hash not published, or file published but cannot be
+                  found. Considering pinning the file using something like{" "}
+                  <a href="https://pinata.cloud/">Pinata</a>.
+                </div>
+              )}
+            </div>
+          )}
+        </>
+      ) : (
+        <div className={styles.fileDrop}>
+          <div>
+            Drop a file here to check against expected hash.
+            <br />
+            <br />
+            <button>Choose File</button>
+          </div>
+          <input
+            className={styles.fileDropInput}
+            type="file"
+            ref={fileRef}
+            onChange={loadFile}
+          />
+        </div>
       )}
     </div>
   );
