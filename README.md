ac8248f76680268f3ce9c21b03f6ca824528b709
Mark Miro <contact@markmiro.com>
2021-08-21 00:33:13 -0700

    Add typescript support for hardhat

---
+++ README.md
@@ -0,0 +1,4 @@
+# How this project was setup
+
+1. Setup basic system using `npx hardhat` and choose "Create a sample project".
+1. [Add TypeScript support](https://hardhat.org/guides/typescript.html#typescript-support).
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
+import { task, HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -13,9 +14,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: "0.8.4",
 };
+
+export default config;
