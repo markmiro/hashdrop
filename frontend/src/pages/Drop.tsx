54a0ef17e9846ba09265755cb842eb0a25629e28
Mark Miro <contact@markmiro.com>
2021-08-25 01:06:10 -0700

    Generate drop id

--- frontend/src/pages/Drop.tsx
+++ frontend/src/pages/Drop.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
 import { Cid } from "../generic/Cid";
 import { ipfsCid } from "../util/ipfsCid";
 import { DataTabs } from "../components/DataTabs";
 
 export function Drop() {
   const [fileOrBlob, setFileOrBlob] = useState<File | Blob | null>(null);
+  const [dropId, setDropId] = useState("");
   const [localCid, setLocalCid] = useState("");
+
+  useEffect(() => {
+    setDropId(uuid());
+  }, []);
 
   async function updateLocalCid(fileOrBlob: File | Blob | null) {
     setFileOrBlob(fileOrBlob);
@@ -30,6 +36,8 @@ export function Drop() {
       <div className="db pa2 bg-black-05">
         Local CID:
         <Cid cid={localCid} />
+        Drop ID:
+        <div>{dropId}</div>
       </div>
     </div>
   );
