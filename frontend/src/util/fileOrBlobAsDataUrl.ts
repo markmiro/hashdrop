c41a75589bc9894026dc10847be1ff3b87a9bd27
Mark Miro <contact@markmiro.com>
2021-08-26 14:29:12 -0700

    Add comment

--- frontend/src/util/fileOrBlobAsDataUrl.ts
+++ frontend/src/util/fileOrBlobAsDataUrl.ts
@@ -1,4 +1,6 @@
 // https://stackoverflow.com/a/20285053
+// Also, this: `window.URL.createObjectURL()`
+// https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
 export function fileOrBlobAsDataUrl(fileOrBlob: File | Blob) {
   return new Promise<string>((resolve, reject) => {
     const reader = new FileReader();
