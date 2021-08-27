2c7c5f8aecd93c59cd6525f575a855bfadaaf023
Mark Miro <contact@markmiro.com>
2021-08-27 15:41:10 -0700

    Add comments to Cover component

--- frontend/src/generic/Cover.tsx
+++ frontend/src/generic/Cover.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react";
 
+// TODO: Consider making this add to a portal, so that it sizes itself to viewport and not parent. Call it <PageCover />
+// TODO: Consider making it prevent parent from scrolling.
+
 export const Cover: FC = ({ children }) => (
   <div
     style={{
       zIndex: 999,
+      // TODO: position: "fixed" makes it cover the whole viewport, but would need to make this a <PageCover /> to make it make sense.
       position: "absolute",
       left: 0,
       top: 0,
