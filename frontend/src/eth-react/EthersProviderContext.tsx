09e258ccedcbcfaf240b60dc3c6725e0632f6be1
Mark Miro <contact@markmiro.com>
2021-09-04 16:22:32 -0700

    Extract testProvider

--- frontend/src/eth-react/EthersProviderContext.tsx
+++ frontend/src/eth-react/EthersProviderContext.tsx
@@ -2,40 +2,8 @@ import { ethers } from "ethers";
 import { createContext, FC, useContext, useEffect, useState } from "react";
 import { useErrorHandler } from "react-error-boundary";
 import { Loader } from "../generic/Loader";
+import { testProvider } from "./testProvider";
 import { useMetaMaskEthereum } from "./useMetaMaskEthereum";
-
-const CONNECTION_TIMEOUT = 5000;
-
-/*
-Try to get block number to test if a connection can be made.
-If it time's out then throw an error.
-*/
-function testProvider(provider: ethers.providers.Web3Provider) {
-  return new Promise<boolean>((resolve, reject) => {
-    let blockNumber: number | null = null;
-
-    const connectionError = () => {
-      const err = Error(
-        "Provider can't connect to network. Please check your MetaMask wallet."
-      );
-      reject(err);
-    };
-
-    provider
-      .getBlockNumber()
-      .then((block) => {
-        blockNumber = block;
-        resolve(true);
-      })
-      .catch(connectionError);
-
-    setTimeout(() => {
-      if (!blockNumber) {
-        connectionError();
-      }
-    }, CONNECTION_TIMEOUT);
-  });
-}
 
 const EthersProviderContext =
   createContext<ethers.providers.Web3Provider | null>(null);
