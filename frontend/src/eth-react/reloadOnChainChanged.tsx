707432182b4c6c8d9a524ce043ec02908e779f25
Mark Miro <contact@markmiro.com>
2021-09-06 02:32:26 -0700

    Fix error when window.ethereum exists but is empty

--- frontend/src/eth-react/reloadOnChainChanged.tsx
+++ frontend/src/eth-react/reloadOnChainChanged.tsx
@@ -1,7 +1,7 @@
 // TODO: put this functionality into an EthereumContext or something
 export function reloadOnChainChanged() {
   // https://docs.metamask.io/guide/ethereum-provider.html#chainchanged
-  if ("ethereum" in window) {
+  if ("ethereum" in window && (window as any).ethereum) {
     (window as any).ethereum.on("chainChanged", () => window.location.reload());
   }
 }
